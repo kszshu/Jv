4755f7bd68317db66c25a5114f2ee34f3e072d44
kzshu <lista@kszs.hu>
2020-03-16 22:19:43 +0100

    Initial commit

---
+++ app/src/main/java/hu/kszs/jv/settings.kt
@@ -0,0 +1,12 @@
+package hu.kszs.jv
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class settings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
